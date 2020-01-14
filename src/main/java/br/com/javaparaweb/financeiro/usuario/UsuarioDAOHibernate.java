f92c02d9ae18fc404d192848a86a41c04274e3bf
André Luiz <alfb1@hotmail.com>
2020-01-14 18:48:47 +0000

    criacao das classes UsuarioDAO e UsuarioDAOHibernate

---
+++ src/main/java/br/com/javaparaweb/financeiro/usuario/UsuarioDAOHibernate.java
@@ -0,0 +1,48 @@
+package br.com.javaparaweb.financeiro.usuario;
+
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+public class UsuarioDAOHibernate implements UsuarioDAO {
+	
+	private Session session;
+	
+	private void setSession(Session session) {
+		this.session = session;
+	}
+
+	public void salvar(Usuario usuario) {
+       this.session.save(usuario);
+	}
+
+	public void atualizar(Usuario usuario) {
+       this.atualizar(usuario);
+	}
+	
+	public void excluir(Usuario usuario) {
+       this.excluir(usuario);
+	}
+	
+	public Usuario carregar(Integer codigo) {
+       return (Usuario) this.session.get(Usuario.class, codigo);
+	}
+	
+	public List<Usuario> listar() {
+		return this.session.createCriteria(Usuario.class).list();
+	}
+	
+	public Usuario buscarPorLogin(String login) {
+		// uma selecao no objeto Usuario
+		String hql = "select u from Usuario u where u.login = :login";
+		// utilizando o session interno para gerar a consulta
+		Query query = this.session.createQuery(hql);
+		// setando o parametro login do hql para o login passado no metodo
+		query.setString("login", login);
+		// retornando apenas um resultado na consulta
+		return (Usuario) query.uniqueResult();
+	}
+
+
+
+}
